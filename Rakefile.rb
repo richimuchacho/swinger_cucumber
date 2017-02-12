40c4256324dcf7e30907a1d9d347cb569dfedc56
Richi Hurtado <richihurtado@gmail.com>
2017-02-12 21:01:04 +0100

    .

--- Rakefile.rb
+++ Rakefile.rb
@@ -3,5 +3,7 @@ require 'cucumber'
 require 'cucumber/rake/task'
 
 Cucumber::Rake::Task.new(:features) do |t|
-  t.cucumber_opts = "test/features --format pretty"
+  #t.cucumber_opts = "test/features --format pretty"
+  #t.cucumber_opts = "test/features --format json -o cucumber.json"
+  t.cucumber_opts = "test/features --format junit -o test/reports"
 end
